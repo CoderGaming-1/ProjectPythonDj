183f018bca7f3789f14704c80a2cc9a69adea14a
Ngan <hongngan03dn@gmail.com>
2024-05-26 15:16:12 +0700

    DebugMode

--- ClinicAppointment/settings.py
+++ ClinicAppointment/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z)u$0e#gkxumi@)5b@y##&#b=n*hh3rk5j7uj259*5yelmk!6^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*'] #['django-clinic-appointment.onrender.com', 'pblsignasl.database.windows.net']
 CSRF_TRUSTED_ORIGINS = ['https://clinicappointment-latest.onrender.com', 'http://127.0.0.1:8000/']
